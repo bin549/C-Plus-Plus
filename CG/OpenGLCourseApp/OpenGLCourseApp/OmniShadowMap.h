9a8a0c733a4f83c5e07a4b9624d9bbe068d6040e
bin549 <549288160@qq.com>
2021-06-23 12:04:22 +0800

    Backup C++\CG\OpenGLCourseApp

---
+++ CG/OpenGLCourseApp/OpenGLCourseApp/OmniShadowMap.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ShadowMap.h"
+
+class OmniShadowMap : public ShadowMap
+{
+public:
+	OmniShadowMap();
+	bool Init(unsigned int width, unsigned int height);
+	void Write();
+	void Read(GLenum TextureUnit);
+	~OmniShadowMap();
+};
